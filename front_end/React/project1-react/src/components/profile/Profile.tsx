c415c6abdc41ba2d683a384bd61e46d8b79621bb
Rakesh Chandraraj <crakesh20@vt.edu>
2024-12-22 11:46:13 -0500

    Profile styling

--- front_end/React/project1-react/src/components/profile/Profile.tsx
+++ front_end/React/project1-react/src/components/profile/Profile.tsx
@@ -3,26 +3,43 @@ import './Profile.css'
 function Profile() {
   return (
     <div>
+        
+        <br />
+        <br />
+        <h1>Profile</h1>
+        <hr />
+
+        <div className="profile_info">
+            <h2>Welcome, Username-of-User!</h2>
+            {/* <h3>Date of Account Creation:</h3> */}
+            {/* <h3>About Me:</h3> */}
+            <h3>Characters Made:</h3>
+        </div>
+
         <div className="left_char">
             {/*<!-- Show the list of all characters made by the user -->
                 <!-- Will be replaced by API calls using JS -->*/}
             <span className="char">
-                <b>Character 1 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
+                <b>*Character 1 Name*</b> {/* The name should replace this <b> tag entirely */} 
+                <br /><br />
+                <b>Age:</b> 
+                <br /><br />
+                <b>Gender:</b> 
+                <br />
+                <br />
+                <b>Description:</b> 
+                <br />
+                <br />
+                <b>Setting:</b>                
+                <br />
+                <br />
+                <b>Private?</b> 
+                <br />
+                <br />
                 <b>18+?</b> <br></br>
             </span>
 
-            <span className="char">
+            {/* <span className="char">
                 <b>Character 2 Name</b> <br>
                 </br>
                 <b>Age:</b> <br>
@@ -84,7 +101,7 @@ function Profile() {
                 <b>Private?</b> <br>            
                 </br>
                 <b>18+?</b> <br></br>
-            </span>
+            </span> */}
         </div>
     </div>
   )
