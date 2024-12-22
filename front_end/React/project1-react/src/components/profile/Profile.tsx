be025dd233852dd4f816ede4a6fdb6546b0e3720
Rakesh Chandraraj <crakesh20@vt.edu>
2024-12-22 15:25:08 -0500

    unfinished profile system

--- front_end/React/project1-react/src/components/profile/Profile.tsx
+++ front_end/React/project1-react/src/components/profile/Profile.tsx
@@ -1,93 +1,72 @@
-import './Profile.css'
+import "./Profile.css";
 
 function Profile() {
   return (
     <div>
-        <div className="left_char">
-            {/*<!-- Show the list of all characters made by the user -->
+      <br /> <br />
+      <h1>Profile</h1>
+      <div className="profile_info">
+        <hr />
+        <h2>Welcome, Username-of-User!</h2>
+        <h3>About Me:</h3>
+        <h3>Date of Profile Creation:</h3>
+        <h3>Characters Made:</h3>
+      </div>
+      <div className="left_char">
+        {/*<!-- Show the list of all characters made by the user -->
                 <!-- Will be replaced by API calls using JS -->*/}
-            <span className="char">
-                <b>Character 1 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
-                <b>18+?</b> <br></br>
-            </span>
+        <span className="char">
+          <b>Character 1 Name</b> <br></br>
+          <b>Age:</b> <br></br>
+          <b>Gender:</b> <br></br>
+          <b>Description:</b> <br></br>
+          <b>Setting:</b> <br></br>
+          <b>Private?</b> <br></br>
+          <b>18+?</b> <br></br>
+        </span>
+        {/*
+        <span className="char">
+          <b>Character 2 Name</b> <br></br>
+          <b>Age:</b> <br></br>
+          <b>Gender:</b> <br></br>
+          <b>Description:</b> <br></br>
+          <b>Setting:</b> <br></br>
+          <b>Private?</b> <br></br>
+          <b>18+?</b> <br></br>
+        </span>
 
-            <span className="char">
-                <b>Character 2 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
-                <b>18+?</b> <br></br>
-            </span>
+        <span className="char">
+          <b>Character 3 Name</b> <br></br>
+          <b>Age:</b> <br></br>
+          <b>Gender:</b> <br></br>
+          <b>Description:</b> <br></br>
+          <b>Setting:</b> <br></br>
+          <b>Private?</b> <br></br>
+          <b>18+?</b> <br></br>
+        </span>
 
-            <span className="char">
-                <b>Character 3 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
-                <b>18+?</b> <br></br>
-            </span>
+        <span className="char">
+          <b>Character 4 Name</b> <br></br>
+          <b>Age:</b> <br></br>
+          <b>Gender:</b> <br></br>
+          <b>Description:</b> <br></br>
+          <b>Setting:</b> <br></br>
+          <b>Private?</b> <br></br>
+          <b>18+?</b> <br></br>
+        </span>
 
-            <span className="char">
-                <b>Character 4 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
-                <b>18+?</b> <br></br>
-            </span>
-
-            <span className="char">
-                <b>Character 5 Name</b> <br>
-                </br>
-                <b>Age:</b> <br>
-                </br>
-                <b>Gender:</b> <br>
-                </br>
-                <b>Description:</b> <br>
-                </br>
-                <b>Setting:</b> <br>
-                </br>
-                <b>Private?</b> <br>            
-                </br>
-                <b>18+?</b> <br></br>
-            </span>
-        </div>
+        <span className="char">
+          <b>Character 5 Name</b> <br></br>
+          <b>Age:</b> <br></br>
+          <b>Gender:</b> <br></br>
+          <b>Description:</b> <br></br>
+          <b>Setting:</b> <br></br>
+          <b>Private?</b> <br></br>
+          <b>18+?</b> <br></br>
+        </span> */}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
